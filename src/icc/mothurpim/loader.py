2a83c945ae50b1715c6981e2ad0f8b32a6593739
Evgeny Cherkashin <eugeneai@irnok.net>
2018-08-26 02:05:04 +0800

    Now it loads some metainfo.

--- src/icc/mothurpim/loader.py
+++ src/icc/mothurpim/loader.py
@@ -27,14 +27,93 @@ class Loader:
 
         findnames = self.sourcedir+"/*"+CPPEXT
         files = glob.glob(findnames)
-        print(findnames)
         for f in files:
             name = f.replace(CPPEXT, "")
-            header = name+HEXT
-            print(f, header)
+            header = glob.glob(name+HEXT+"*")[0]
+            # print(f, header)
+            fl = CommandLoader(self, f, header)
+            fl.load()
 
         self.loaded = True
         return self.graph
+
+
+# NAME = "[a-zA-Z.]+"
+NAME = ".+?"
+
+
+def re_simple(methodname):
+    s = 'get'+methodname+r'\(\s*\)\s*{\s*return\s+"('+NAME+r')"\s*;\s*}'
+    return re.compile(s)
+
+
+RE_NAME = re_simple("CommandName")
+RE_CITE = re_simple("Citation")
+RE_CAT = re_simple("CommandCategory")
+RE_DESCR = re_simple("Description")
+
+
+class CommandLoader:
+    def __init__(self, loader, cpp, header):
+        self.loader = loader
+        self.cpp = cpp
+        self.h = header
+
+    def load(self):
+        self.loadh()
+        self.loadcpp()
+
+    def loadh(self):
+        self.text = open(self.h).read()
+        name = self.find(RE_NAME, "name")
+        category = self.find(RE_CAT, "category")
+        citation = self.find(RE_CITE, "citation")
+        description = self.find(RE_DESCR, "description")
+        print(f"{name}:{category}\n {citation}\n {description}")
+
+    def find(self, re, ent):
+        m = re.search(self.text)
+        if m is not None:
+            value = m.group(1)
+        else:
+            raise ValueError(ent+" not found")
+        return value
+
+    def loadcpp(self):
+        pass
+
+# class CommandParameter {
+
+# 	public:
+#     CommandParameter() { name = ""; type = ""; options = ""; optionsDefault = ""; chooseOnlyOneGroup = ""; chooseAtLeastOneGroup = ""; linkedGroup = ""; multipleSelectionAllowed = false; required = false; important = false; outputTypes = ""; }
+#     CommandParameter(string n, string t, string o, string d, string only, string atLeast, string linked, string opt, bool m, bool r, bool i) :
+#         name(n), type(t), options(o), optionsDefault(d), chooseOnlyOneGroup(only), chooseAtLeastOneGroup(atLeast), linkedGroup(linked), outputTypes(opt),multipleSelectionAllowed(m), required(r), important(i) {}
+#     CommandParameter(string n, string t, string o, string d, string only, string atLeast, string linked, string opt, bool m, bool r) : name(n), type(t), options(o), optionsDefault(d),
+#             chooseOnlyOneGroup(only), chooseAtLeastOneGroup(atLeast), linkedGroup(linked), outputTypes(opt), multipleSelectionAllowed(m), required(r)  { important = false; }
+
+# 		string name;		//something like fasta, processors, method
+# 		string type;  //must be set to "Boolean", "Multiple", "Number", "String", "InputTypes" - InputTypes is for file inputs
+# 		string options; //if the parameter has specific options allowed, used for parameters of type "Multiple", something like "furthest-nearest-average", or "sobs-chao...", leave blank for command that do not required specific options
+# 		string optionsDefault;   //the default for this parameter, could be something like "F" for a boolean or "100" for a number or "sobs-chao" for multiple
+
+
+# 		//for chooseOnlyOneGroup, chooseAtLeastOneGroup and linkedGroup if no group is needed set to "none".
+# 		string chooseOnlyOneGroup; //for file inputs: if a command has several options for input files but you can only choose one then put them in a group
+# 									//for instance in the read.dist command you can use a phylip or column file but not both so set chooseOnlyOneGroup for both parameters to something like "DistanceFileGroup"
+# 		string chooseAtLeastOneGroup; //for file inputs: if a command has several options for input files and you want to make sure one is choosen then put them in a group
+# 									//for instance in the read.dist command you must provide a phylip or column file so set chooseAtLeastOneGroup for both parameters to something like "DistanceFileGroup"
+# 		string linkedGroup; //for file inputs: if a command has a file option were if you provide one you must provide another you can put them in a group
+# 										//for instance in the cluster command if you provide a column file you must provide a name file so set linkedGroup for both parameters to something like "ColumnNameGroup"
+
+# 		bool multipleSelectionAllowed; //for "Multiple" type to say whether you can select multiple options, for instance for calc parameter set to true, but for method set to false
+# 		bool required; //is this parameter required
+
+#         bool important; //is this parameter important.  The gui will put "important" parameters first in the option panel.
+
+#         string outputTypes; //types on files created by the command if this parameter is given.  ie. get.seqs command fasta parameter makes a fasta file. can be multiple values split by dashes.
+
+# 	private:
+# };
 
 
 def rdflib_example():
